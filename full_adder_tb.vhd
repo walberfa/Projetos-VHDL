6b5ab528c5ab973b3ead677a67b6859afa62e552
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2020-08-30 14:16:48 -0300

    Create full_adder_tb.vhd

---
+++ full_adder_tb.vhd
@@ -0,0 +1,71 @@
+--test bench
+
+library ieee;
+use ieee.std_logic_114.all;
+
+entity full_adder_tb is
+end full_adder_tb;
+
+--cria sinais lógicos para representar as entradas e saídas
+architecture behave of full_adder_tb is
+	signal A: std_logic:= '0';
+	signal B: std_logic:= '0';
+  signal Cin: std_logic: = '0';
+	signal Sum: std_logic;
+	signal Carry: std_logic;
+
+--faz a chamada do "full.adder" já criado e associa as portas
+begin
+	UUT: entity work.full_adder
+		port map (
+			A => A;
+			B => B;
+      Cin => Cin;
+			Sum => Sum;
+			Carry => Carry
+		);
+    
+-- implementa os testes
+	process is
+	begin
+		A <= '0';
+		B <= '0';
+    Cin <= '0';
+		wait for 10ns;
+
+		A <= '0';
+		B <= '1';
+    Cin <= '0';
+		wait for 10ns;
+
+		A <= '1';
+		B <= '0';
+    Cin <= '0';
+		wait for 10ns;
+
+		A <= '1';
+		B <= '1';
+    Cin <= '0';
+		wait for 10ns;
+    
+		A <= '0';
+		B <= '0';
+    Cin <= '1';
+		wait for 10ns;
+
+		A <= '0';
+		B <= '1';
+    Cin <= '1';
+		wait for 10ns;
+
+		A <= '1';
+		B <= '0';
+    Cin <= '1';
+		wait for 10ns;
+
+		A <= '1';
+		B <= '1';
+    Cin <= '1';
+		wait for 10ns;
+	end process
+end behave;
